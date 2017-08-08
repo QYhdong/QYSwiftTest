9e699d3701c8c1f284c13992f7efc2c390345a14
qingyun <809184372@qq.com>
2017-08-08 14:45:12 +0800

    方法柯里化

--- QYSwiftTest/QYSwiftTest/ViewController.swift
+++ QYSwiftTest/QYSwiftTest/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let addTwo = addTo(adder: 2)
+        let result = addTwo(6)
+        print("方法currying后结果是\(result)")
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    //柯里化方法
+    func addTo(adder:Int) -> (Int) -> Int{
+        return {
+            num in
+            return num + adder
+        }
+    }
 
 }
 
