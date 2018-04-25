fb09fdd63c79b381915ea4a782569d0ed3aad3f5
QYhdong <809184372@qq.com>
2018-04-25 17:33:44 +0800

    相加求和算法

---
+++ QYSwiftTest/QYSwiftTest/HDAlgorithmViewController.swift
@@ -0,0 +1,37 @@
+//
+//  HDAlgorithmViewController.swift
+//  QYSwiftTest
+//
+//  Created by HJddd on 2018/4/25.
+//  Copyright © 2018年 HuangDong. All rights reserved.
+//
+
+import UIKit
+
+class HDAlgorithmViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let numArr = [2,7,20,12,8,3]
+        algorithmOne(numArr, 9)
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+    
+//相加求和
+    
+/*
+ 
+ */
+    
+    func algorithmOne(_ num:[Int], _ target:Int)  {
+        
+    }
+
+    
+}
