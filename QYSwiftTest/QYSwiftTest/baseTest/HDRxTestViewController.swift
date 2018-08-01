082853e56443eba5cc436f6490fc79e4974b9aff
QYhdong <809184372@qq.com>
2018-08-01 18:26:09 +0800

    add.

---
+++ QYSwiftTest/QYSwiftTest/baseTest/HDRxTestViewController.swift
@@ -0,0 +1,36 @@
+
+//
+//  HDRxTestViewController.swift
+//  QYSwiftTest
+//
+//  Created by HJddd on 2018/8/1.
+//  Copyright © 2018年 HuangDong. All rights reserved.
+//
+
+import UIKit
+
+class HDRxTestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
