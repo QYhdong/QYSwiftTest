74cf31c9745a0136404326cc23e09618440c2c08
QYhdong <809184372@qq.com>
2018-08-14 14:09:47 +0800

    ms

--- QYSwiftTest/QYSwiftTest/AppDelegate.swift
+++ QYSwiftTest/QYSwiftTest/AppDelegate.swift
@@ -48,3 +48,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+/*
+     •iOS事件传递及响应者链条： 1.当iOS程序中发生触摸事件后，系统会将事件加入到UIApplication管理的一个任务队列中 2.UIApplication将处于任务队列最前端的事件向下分发。即UIWindow。 3.UIWindow将事件向下分发，即UIView。 4.UIView首先看自己是否能处理事件，触摸点是否在自己身上。如果能，那么继续寻找子视图。 5.遍历子控件，重复以上两步。 6.如果没有找到，那么自己就是事件处理者。如果 7.如果自己不能处理，那么不做任何处理。
+ */
