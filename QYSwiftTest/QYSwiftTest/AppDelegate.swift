a65599afbef87a6b66652920fc796fec8c1e4604
QYhdong <809184372@qq.com>
2018-09-27 18:28:50 +0800

    RXSwift 数字相加

--- QYSwiftTest/QYSwiftTest/AppDelegate.swift
+++ QYSwiftTest/QYSwiftTest/AppDelegate.swift
@@ -17,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        self.window?.rootViewController = HDRxTestViewController()
+        let numberVc = UIStoryboard.init(name: "RXStoryboard", bundle: nil).instantiateViewController(withIdentifier: "HDRXNumberViewController")
+        self.window?.rootViewController = numberVc
         self.window?.makeKeyAndVisible()
         
         return true
--- QYSwiftTest/QYSwiftTest/RXSwift /HDRXNumberViewController.swift
+++ QYSwiftTest/QYSwiftTest/RXSwift /HDRXNumberViewController.swift
@@ -8,13 +8,30 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class HDRXNumberViewController: UIViewController {
-
+    
+    @IBOutlet weak var numOneTextField: UITextField!                //参与计算数字1
+    @IBOutlet weak var numTwoTextField: UITextField!                //参与计算数字2
+    @IBOutlet weak var numThreeTextField: UITextField!             //参与计算数字3
+    @IBOutlet weak var resultLabel: UILabel!                                //结果
+    
+    var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        Observable.combineLatest(numOneTextField.rx.text.orEmpty,numTwoTextField.rx.text.orEmpty,numThreeTextField.rx.text.orEmpty){
+            textValue1, textValue2, textValue3 -> Int   in
+            return (Int(textValue1) ?? 0)+(Int(textValue2) ?? 0)+(Int(textValue3) ?? 0)
+        }
+        .map{$0.description}
+        .bind(to: resultLabel.rx.text)
+        .disposed(by: disposeBag)
+        
+        
     }
     
 
