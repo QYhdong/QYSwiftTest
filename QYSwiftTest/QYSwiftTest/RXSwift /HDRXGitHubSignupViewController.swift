7bc445860e2e82f1309d3afae9841b6031d640b0
QYhdong <809184372@qq.com>
2018-09-28 10:14:14 +0800

    HDRXGitHubSignupViewController

--- QYSwiftTest/QYSwiftTest/RXSwift /HDRXGitHubSignupViewController.swift
+++ QYSwiftTest/QYSwiftTest/RXSwift /HDRXGitHubSignupViewController.swift
@@ -12,8 +12,11 @@ import SnapKit
 class HDRXGitHubSignupViewController: UIViewController {
 
     lazy var userNameTextField:UITextField! = UITextField()                                         //用户名
+    lazy var usernameValidation = UILabel()                                                                       //用户名验证
     lazy var passwordTextField:UITextField! = UITextField()                                         //密码
+    lazy var passwordValidation = UILabel()                                                                       //密码验证
     lazy var repeatedPasswordTextField:UITextField! = UITextField()                          //确认密码
+    lazy var repeatedPasswordValidation = UILabel()                                                        //确认密码验证
     
     lazy var signupBtn:UIButton = {                                                                                   //登录按钮
         let btn = UIButton(type: UIButtonType.custom)
@@ -29,13 +32,19 @@ class HDRXGitHubSignupViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    //创建布局
+    //布局
     func setupUI() {
         
         view.addSubview(userNameTextField)
         view.addSubview(passwordTextField)
         view.addSubview(repeatedPasswordTextField)
         view.addSubview(repeatedPasswordTextField)
+        
+        userNameTextField.placeholder = "Username(用户名)"
+        passwordTextField.placeholder = "Password(密码)"
+        repeatedPasswordTextField.placeholder = "Password Repeat(确认密码)"
+        
+        
         
     }
 
