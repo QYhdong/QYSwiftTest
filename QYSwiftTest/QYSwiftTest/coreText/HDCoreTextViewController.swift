ec913ad1441457bfeef79f34597dd5332a8102c1
QYhdong <809184372@qq.com>
2018-12-07 10:10:53 +0800

    coretext练习

---
+++ QYSwiftTest/QYSwiftTest/coreText/HDCoreTextViewController.swift
@@ -0,0 +1,27 @@
+//
+//  HDCoreTextViewController.swift
+//  QYSwiftTest
+//
+//  Created by HJddd on 2018/12/7.
+//  Copyright © 2018年 HuangDong. All rights reserved.
+//
+
+import UIKit
+
+class HDCoreTextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func setupUI(){
+        
+    }
+    
+    
+
+}
